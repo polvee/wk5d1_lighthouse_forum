7eaafa21e117d427c670db1a130e93d6a2ef9dc8
vagrant <paul.val1@yahoo.ca>
2014-06-02 19:23:57 +0000

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wk5d1LighthouseForum::Application.config.secret_key_base = '1a2041dc82f51181b28b9bf82a07b87979335d17d5975aa636c8ed334e5e73c857b562c305a4c76253e1cacacb35cebf0741868971995ce8e5345c9ad8a3a236'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wk5d1LighthouseForum::Application.config.session_store :cookie_store, key: '_wk5d1_lighthouse_forum_session'
