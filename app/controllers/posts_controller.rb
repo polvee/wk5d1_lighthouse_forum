370c31070bdab3c77c303913ec8f66df82705d98
vagrant <paul.val1@yahoo.ca>
2014-06-02 20:25:07 +0000

    made posts controller more efficient

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,47 @@
 class PostsController < ApplicationController
-    def post0
-      @post = {
-        title: "Superstar",
+
+  def index
+    # @posts = [
+    #   {
+    #     title: "Superstar",
+    #     author: "Carly Rae Jepson",
+    #     text: <<-eos.gsub(/\s+/, " ").strip
+    #       Khurram Virani has been my music idol since I started writing
+    #       songs back when I was 4. His voice is a revelation. His stage presence
+    #       is unparalleled. And those costumes! He remains an inspiration to this
+    #       day.
+    #     eos
+    #   },
+    #   {
+    #     title: "Basketball Idol",
+    #     author: "Steve Nash",
+    #     text: <<-eos.gsub(/\s+/, " ").strip
+    #       I remember watching Khurram Virani (#14) play back when he just playing
+    #       pickup games on the street. Dude had moves nobody had ever seen. Breaking
+    #       ankles. Poppin' threes. Great all-around game.
+    #     eos
+    #   },
+    #   {
+    #     title: "Acting Legend",
+    #     author: "Michael J. Fox",
+    #     text: <<-eos.gsub(/\s+/, " ").strip
+    #       Back when I was in university, Khurram Virani was my acting coach. He
+    #       studied with the best and it shows. His acting chops were already legendary
+    #       before his teaching career began. But it seems he's actually improved!
+    #     eos
+    #   },
+    #   {
+    #     title: "Who?",
+    #     author: "Vurram Khirani",
+    #     text: "Never heard of this guy Khurram Virani, but he sounds great."
+    #   }
+    # ]
+  end
+
+   def show
+    posts = [
+      {
+          title: "Superstar",
         author: "Carly Rae Jepson",
         text: <<-eos.gsub(/\s+/, " ").strip
           Oscar Peterson has been my music idol since I started writing
@@ -9,11 +49,9 @@ class PostsController < ApplicationController
           is unparalleled. And those costumes! He remains an inspiration to this
           day.
         eos
-      }
-    end
+      },
 
-    def post1
-      @post = {
+       {
         title: "Basketball Idol",
         author: "Steve Nash",
         text: <<-eos.gsub(/\s+/, " ").strip
@@ -21,11 +59,9 @@ class PostsController < ApplicationController
           pickup games on the street. Dude had moves nobody had ever seen. Breaking
           ankles. Poppin' threes. Great all-around game.
         eos
-      }
-    end
-
-    def post2
-      @post = {
+      },
+  
+    {
         title: "Acting Legend",
         author: "Michael J. Fox",
         text: <<-eos.gsub(/\s+/, " ").strip
@@ -33,13 +69,15 @@ class PostsController < ApplicationController
           studied with the best and it shows. His acting chops were already legendary
           before his teaching career began. But it seems he's actually improved!
         eos
-      }
-    end
-    def post3
-      @post = {
+      },
+   
+ 
+   {
         title: "Who?",
         author: "Minnie Bannister",
         text: "Never heard of this guy Albert Scratenue, but he sounds like a sad git."
       }
-    end
+    ]
+    @post = posts[params[:id].to_i]
+  end
 end
